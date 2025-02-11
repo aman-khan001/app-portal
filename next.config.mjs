98f8bb4a2aac89617d8e3f449238d35883206d58
Aman Khan <amanazadkhan.ak@gmail.com>
2025-02-11 13:06:28 +0530

    updated config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/app-portal",
-  assetPrefix: "/app-portal/",
-  images: {
-    unoptimized: true, // Required for GitHub Pages if using images
-  },
-};
-
-export default nextConfig;
+    output: "export",
+    basePath: "/app-portal",
+    assetPrefix: "/app-portal/",
+    images: {
+      unoptimized: true, // Required for GitHub Pages if using images
+    },
+  };
+  
+  export default nextConfig;
+  
